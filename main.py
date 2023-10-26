417fda1e9b364f74ffe070b949a5069bbdb99704
Anubha Sharma <anubhaa.sharma27@gmail.com>
2023-10-26 23:37:24 +0530

    addimg agent files and completed linkedin scraping

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from langchain.prompts import PromptTemplate 
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain 
-
+from third_parties.linkedin import scrape_linkedin_profile
 import os 
 
 information ="""
@@ -9,6 +9,7 @@ When Modi was eight years old, he was introduced to the Rashtriya Swayamsevak Sa
 """
 if __name__ == '__main__':
     print("Hello World")
+
     summary_template = """
     I want you to create a short summary of the given information {information}
     """
@@ -16,12 +17,17 @@ if __name__ == '__main__':
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
+    
     llm = ChatOpenAI(
         temperature=0,
         model_name="gpt-3.5-turbo"
     )
+    
     chain = LLMChain(
         llm=llm,
         prompt=summary_prompt_template
     )
-    print(chain.run(information= information ))
+    
+    linkedin_data = scrape_linkedin_profile(profile_url="https://www.linkedin.com/in/divyanshu-rana-792a03223/")
+    
+    print(chain.run(information= linkedin_data ))
